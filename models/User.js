ff3af37e933c2baf3701836a2ee7d21ffd265850
Gravity <tevoned@gmail.com>
2022-03-16 22:51:16 -0400

    password feature

--- models/User.js
+++ models/User.js
@@ -34,7 +34,7 @@ User.init(
     }
   },
   {
-    
+
     hooks: {
       // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newUserData) {
@@ -56,5 +56,13 @@ User.init(
     modelName: 'user'
   }
 );
+
+// create our User model
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
   
 module.exports = User;
